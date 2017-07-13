eca193ec049f3ee286436d079542542faa9f7b56
Cody Daniels <codydaniels@gmail.com>
2017-07-13 12:27:59 -0700

    Refresh background image on wotd update

--- public/index.js
+++ public/index.js
@@ -2,6 +2,6 @@ $.ajax({
   url: '/wotd',
   method: 'GET'
 }).done(function(res) {
-  $('body').css('background-image', 'url(thumbnail.png)');
+  $('body').css('background-image', 'url(thumbnail.png?random=' + new Date().getTime() + ')');
   $('a').attr('href', res.url);
 });
